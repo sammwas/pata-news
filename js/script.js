8d6ab79769be13b2d672e1f9b1ae7849052d834e
waracci <morriswarachi0@gmail.com>
2017-07-07 17:47:29 +0300

    added first news article

--- js/script.js
+++ js/script.js
@@ -8,19 +8,41 @@ $(document).ready(function (){
         $.ajax({
             url: 'https://newsapi.org/v1/sources'
         }).done(function (sourcesPlace){
-            console.log(sourcesPlace);
+        //    console.log(sourcesPlace);
             for(var i = 0; i<sourcesPlace.sources.length;i++){
-            console.log(sourcesPlace.sources[i].category);
+           //news sources
         }
         });
-        console.log(news.articles.length);
+        console.log(news.articles[0]);
         for(var i = 0; i<news.articles.length;i++){
-            console.log(news.articles[i].author);
-            console.log(news.articles[i].title);
-            console.log(news.articles[i].description);
-            console.log(news.articles[i].url);
-            console.log(news.articles[i].urlToImage);
-            console.log(news.articles[i].publishedAt);
+             $('#newsOutlet').html(`
+            <div class="col-md-4">
+                <div class="thumbnail">
+                    <img src="${news.articles[0].urlToImage}" alt="" class="img-responsive">
+                    <div class="caption">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <h4 class="text-primary">${news.articles[0].title}</h4>
+                            </div>
+                            <div class="col-md-6">
+                                <p class="">date posted: ${news.articles[0].publishedAt}</p>
+                            </div>
+                        </div>
+                         <hr>  
+                         <p>${news.articles[0].description}</p> <hr>
+                         <div class='row'>
+                         <div class='col-md-6'>
+                         Author: ${news.articles[0].author}
+                         Source: ${news.source}
+                         </div>
+                         <div class='col-md-6'>
+                         <a href=" ${news.articles[0].url}" class="btn btn-warning" target='_blank'>Read more</a>
+                         </div>
+                         </div>
+                    </div>
+                </div>
+            </div>
+            `);
         }
 
 
